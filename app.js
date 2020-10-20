5657b8897cf496181a978420a155f523e13195fb
Dariss Harris <darissharris@gmail.com>
2020-10-20 12:36:55 -0700

    refined updates

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
+console.log(process.env.API_TOKEN);
+
 const app = express();
-const data = require('moviedata');
+
 
 app.use(morgan('common')); // let's see what 'common' format looks like
 
-app.get('/', (req, res) => {
-  // ALL OUR CODE HERE
-});
+const movieData = require('./moviedata.js');
+
+function handleGetMovie(req, res){
+  res.json(movieData);
+}
+
+app.get('/movie', handleGetMovie); 
 
 app.listen(8000, () => {
   console.log('Server started on PORT 8000');
