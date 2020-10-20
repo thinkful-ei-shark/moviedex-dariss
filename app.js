e2fff9b7a609868d1f13a028c8fee3be4a7d5902
Dariss Harris <darissharris@gmail.com>
2020-10-20 14:48:30 -0700

    api server working

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const movieInfo = require('./movieData.json');
 
-console.log(process.env.API_TOKEN);
 
 const app = express();
 
 
 app.use(morgan('common')); // let's see what 'common' format looks like
 
-const movieData = require('./moviedata.js');
 
-function handleGetMovie(req, res){
-  res.json(movieData);
-}
 
-app.get('/movie', handleGetMovie); 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
-app.listen(8000, () => {
-  console.log('Server started on PORT 8000');
+app.get('/movie', function handleGetMovie(req, res) {
+  let response = movieInfo;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
